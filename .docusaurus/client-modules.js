7c0a8c8900272ac5085c25818718b5bb44a7253e
musinadasunil <sunil.musinada@gmail.com>
2025-05-18 21:24:00 +1000

    Added Docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/sunilmusinada/Projects/mljourney.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/sunilmusinada/Projects/mljourney.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/sunilmusinada/Projects/mljourney.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/sunilmusinada/Projects/mljourney.github.io/src/css/custom.css"),
+];
